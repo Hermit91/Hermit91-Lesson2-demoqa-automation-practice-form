27ca93dd8279e84aaa79df20f2aa3aebf6813ad0
Hermit91 <work.salokin@gmail.com>
2021-10-28 18:53:28 +0300

    added: birth date, subject

--- src/test/java/gmail/salokin1991/TestPracticeForm.java
+++ src/test/java/gmail/salokin1991/TestPracticeForm.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestPracticeForm {
 
@@ -25,8 +29,10 @@ public class TestPracticeForm {
         String uLN = "P";               //last name
         String uE = "Some@a.com";       //email
         String mN = "1234567890";       //mobile number
-        String cD = "28 Oct 2021";      //current Date
-        String bD = "7 oct 1952";       //birth date
+        String bD = "27";                //birth day
+//        String bD = "7";                //birth day error
+        String bM = "10";               //birth month
+        String bY = "1952";             //birth year
         String iSubj = "History";       //subject
         String cA = "Covid bunker";     //current address
 
@@ -47,7 +53,26 @@ public class TestPracticeForm {
         $("#userEmail").setValue(uE);           //input email
         $("#userNumber").setValue(mN);          //input mobile number
         $("#currentAddress").setValue(cA);      //input address
-        
+
+        $("#dateOfBirthInput").click();         //input birth date
+        $(".react-datepicker__month-select").selectOptionByValue(bM);
+        $(".react-datepicker__year-select").selectOptionByValue(bY);
+        $$(".react-datepicker__day").find(text(bD)).click();
+
+
+        $("#subjectsInput").click();            //input subject
+        $("#subjectsInput").setValue(iSubj).pressEnter();
+
+
+
+//        File image = new File(meme);
+//        $("#uploadPicture").uploadFile(image);
+
+//        $("#state").click();
+//        $(byText(cState)).click();
+//        $("#city").click();
+//        $(byText(cCity)).click();
+//        $("#submit").click();
 
 
     }
