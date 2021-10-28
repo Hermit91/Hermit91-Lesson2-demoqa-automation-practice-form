eb802346d9f29c2e137f62ad06c53f54f8b6b7d9
Hermit91 <work.salokin@gmail.com>
2021-10-29 01:23:30 +0300

    xpath date selection

--- src/test/java/gmail/salokin1991/TestPracticeForm.java
+++ src/test/java/gmail/salokin1991/TestPracticeForm.java
@@ -29,9 +29,8 @@ public class TestPracticeForm {
         String uLN = "P";                   //last name
         String uE = "Some@a.com";           //email
         String mN = "1234567890";           //mobile number
-        String bD1 = "27";                  //birth day
-        String bD2 = "7";                   //birth day error
-        String bM = "10";                   //birth month
+        String bD = "07";                   //birth day error
+        String bM = "9";                    //birth month 10-1
         String bY = "1952";                 //birth year
         String iSubj1 = "History";          //subject
         String iSubj2 = "Sports";           //subject
@@ -56,37 +55,35 @@ public class TestPracticeForm {
         $("#userNumber").setValue(mN);          //input mobile number
         $("#currentAddress").setValue(cA);      //input address
 
-        $("#dateOfBirthInput").click();         //click birth date
-        $(".react-datepicker__month-select").selectOptionByValue(bM);                   //input month
-        $(".react-datepicker__year-select").selectOptionByValue(bY);                    //input year
+        $("#dateOfBirthInput").click();         //input birth date
+        $x("/html/body/div[2]/div/div/div[2]/div[2]/div[1]/form/div[5]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[1]/select").selectOptionByValue(bM);
+        $x("/html/body/div[2]/div/div/div[2]/div[2]/div[1]/form/div[5]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[2]/select").selectOptionByValue(bY);
+        $x("/html/body/div[2]/div/div/div[2]/div[2]/div[1]/form/div[5]/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div[3]").click();
 
-        $$(".react-datepicker__day").find(text(bD1)).click();                           //input birth day ok
-//error        $$x("/html/body/div[2]/div/div/div[2]/div[2]/div[1]/form/div[5]/div[2]/div[2]/div[2]/div/div/div[2]").find(text(bD2)).click();
-
-        $(byText(g)).click();                              //input gender
+        $(byText(g)).click();                             //input gender
 
         $("#subjectsInput").click();             //input subject
         $("#subjectsInput").setValue(iSubj1).pressEnter();
         $(byText(iSubj2)).click();
 
-        $("#submit").scrollIntoView(true);      //view submit button
+        $("#submit").scrollIntoView(true);       //view submit button
 
-        $("#state").click();                    //input state and city
+        $("#state").click();                     //input state and city
         $(byText(cState)).click();
         $("#city").click();
         $(byText(cCity)).click();
 
-        File image = new File(meme);                     //upload image
+        File image = new File(meme);                       //upload image
         $("#uploadPicture").uploadFile(image);
 
-        $("#submit").click();                   //click submit
+        $("#submit").click();                     //click submit
 
         /////Check test\\\\\
 
         $x("/html/body/div[4]/div/div").shouldHave(text("Thanks for submitting the form"),
-                text(uFN+" "+uLN), text(uE), text(g), text(mN), text(/*bD2*/bD1 + " October," + bY), text(iSubj1),
+                text(uFN+" "+uLN), text(uE), text(g), text(mN), text(bD + " October," + bY), text(iSubj1),
                 text(iSubj2), text("meme.jpg"), text(cA), text(cState+" "+cCity));
 
-        $("#closeLargeModal").click();          //close test
+        $("#closeLargeModal").click();            //close test
     }
 }
